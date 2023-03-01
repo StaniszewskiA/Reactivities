13f0e52d5a96cfa4a39ecb239e0cda54eb37e350
AStaniszewski <staniszewski01adam@gmail.com>
2023-03-01 12:51:15 +0100

    Mediator setup

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,4 +1,6 @@
+using Application.Activities;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -7,24 +9,24 @@ namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
+        private readonly IMediator _mediator;
 
-        private readonly DataContext _context;
-
-        public ActivitiesController(DataContext context)
+        public ActivitiesController(IMediator mediator)
         {
-            _context = context;            
+            _mediator = mediator;
+                     
         }
 
         [HttpGet] //api/activities
         public async Task<ActionResult<List<Activity>>> GetActivitiies()
         {
-            return await _context.Activities.ToListAsync();
+            return await _mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")] //api/acitivities/{id}
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await _context.Activities.FindAsync(id);
+            return Ok();
         }
     }
 }
