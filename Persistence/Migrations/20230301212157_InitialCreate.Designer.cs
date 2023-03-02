ff2e832a3dd8e0c140c9dc947113fd1d3b64ede5
AStaniszewski <staniszewski01adam@gmail.com>
2023-03-02 13:40:41 +0100

    Migrations

--- Persistence/Migrations/20230301212157_InitialCreate.Designer.cs
+++ Persistence/Migrations/20230301212157_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using Persistence;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20230227142736_InitialCreate")]
+    [Migration("20230301212157_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
